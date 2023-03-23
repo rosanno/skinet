eb7243d609917668c08413c9e774411c8e5d8f82
rosanno <rosannomacabuhay@gmail.com>
2023-03-23 22:21:59 +0800

    Fix Product brand and type that return null value

---
+++ Core/Entities/ProductBrand.cs
@@ -0,0 +1,9 @@
+using Core.Entities;
+
+namespace CORE.Entities
+{
+    public class ProductBrand : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Core/Entities/ProductType.cs
@@ -0,0 +1,9 @@
+using Core.Entities;
+
+namespace CORE.Entities
+{
+    public class ProductType : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
