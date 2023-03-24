f3d0f177c7f5a1b2d43e0e87b3f5e31311615ea8
rosanno <rosannomacabuhay@gmail.com>
2023-03-24 13:53:00 +0800

    Add static files

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,20 @@
+using System.Linq.Expressions;
+using CORE.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpicification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
